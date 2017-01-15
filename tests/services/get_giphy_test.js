92176cf24041286f653d5eebcbbef1dcd876b54a
Tamara Temple <tamouse@gmail.com>
2017-01-15 11:46:45 -0600

    Work in progress on giphy API

---
+++ tests/services/get_giphy_test.js
@@ -0,0 +1,15 @@
+const expect = require('expect.js');
+
+const config = require('./../../config');
+const get_giphy = require('./../../services/get_giphy');
+
+describe('get_giphy tests', function () {
+
+    it('returns a json data block', function (done) {
+        get_giphy.fetch_giphy(['funny', 'cat'], function (data) {
+            console.log("data: " + JSON.stringify(data));
+            expect(data).to.be.ok();
+            done();
+        });
+    });
+});
